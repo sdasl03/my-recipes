09b179e5f58463fd9de14fea3969759c191373f4
Samuel Lira <sdasl03@github.com>
2025-02-19 15:17:40 +0000

    first commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+  filteredList: string[] = [];
+
+  constructor(){
+    
+  }
+}
